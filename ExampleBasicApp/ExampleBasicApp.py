18c01161797a75ef6abd0b9ec69e28204e799672
simone.deponti <simone.deponti@gmail.com>
2015-11-27 19:11:01 +0100

    Prima versione

---
+++ ExampleBasicApp/ExampleBasicApp.py
@@ -0,0 +1,36 @@
+﻿from xml.etree import ElementTree
+
+
+def parseXML(xml_file):
+    tree = ElementTree.parse(xml_file)
+    root = tree.getroot()
+    assert root.tag == 'contacts'
+    contacts = []
+    for contact_node in root.iter('contact'):
+        contact = {
+            'id': contact_node.attrib['id']
+        }
+        for property_node in contact_node.iter():
+            if property_node.tag == 'company':
+                contact['company'] = {
+                    'name': property_node.get('name'),
+                    'roles': []
+                }
+                for role_node in property_node.iter('role'):
+                    contact['company']['roles'].append(role_node.text)
+            else:
+                contact[proprty_node.tag] = property_node.text
+        contacts.append(contact)
+    meta = {}
+    meta_node = root.find('/meta')
+    if meta_node:
+        for property_node in meta_node.iter():
+            meta[property_node.tag] = (
+                int(property_node.text.strip(), 10)
+                if property_node.tag == 'txn'
+                else property_node.text
+            )
+    return (contacts, meta)
+
+
+print(parseXML('contacts.xml'))
